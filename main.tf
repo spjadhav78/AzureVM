cdecef12854e185dd89721582faba0295e5bb645
Sanjay Jadhav <spjadhav78@gmail.com>
2023-05-12 23:40:33 +0530

    maintfchanges

--- main.tf
+++ main.tf
@@ -2,39 +2,39 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resource-group"
+resource "azurerm_resource_group" "wintech" {
+  name     = "wintech-resource-group"
   location = "East US"
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "example-vnet"
+resource "azurerm_virtual_network" "wintech" {
+  name                = "wintech-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.wintech.location
+  resource_group_name = azurerm_resource_group.wintech.name
 }
 
-resource "azurerm_subnet" "example" {
-  name                 = "example-subnet"
+resource "azurerm_subnet" "wintech" {
+  name                 = "wintech-subnet"
   address_prefix       = "10.0.1.0/24"
-  virtual_network_name = azurerm_virtual_network.example.name
-  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.wintech.name
+  resource_group_name  = azurerm_resource_group.wintech.name
 }
 
-resource "azurerm_network_security_group" "example" {
-  name                = "example-nsg"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_network_security_group" "wintech" {
+  name                = "wintech-nsg"
+  location            = azurerm_resource_group.wintech.location
+  resource_group_name = azurerm_resource_group.wintech.name
 }
 
-resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_network_interface" "wintech" {
+  name                = "wintech-nic"
+  location            = azurerm_resource_group.wintech.location
+  resource_group_name = azurerm_resource_group.wintech.name
 
   ip_configuration {
-    name                          = "example-ipconfig"
-    subnet_id                     = azurerm_subnet.example.id
+    name                          = "wintech-ipconfig"
+    subnet_id                     = azurerm_subnet.wintech.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = null
   }
@@ -44,10 +44,10 @@ resource "azurerm_network_interface" "example" {
   }
 }
 
-resource "azurerm_public_ip" "example" {
-  name                = "example-public-ip"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_public_ip" "wintech" {
+  name                = "wintech-public-ip"
+  location            = azurerm_resource_group.wintech.location
+  resource_group_name = azurerm_resource_group.wintech.name
   allocation_method   = "Dynamic"
 
   tags = {
@@ -55,10 +55,10 @@ resource "azurerm_public_ip" "example" {
   }
 }
 
-resource "azurerm_storage_account" "example" {
-  name                     = "examplestorageaccount"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+resource "azurerm_storage_account" "wintech" {
+  name                     = "wintechstorageaccount"
+  resource_group_name      = azurerm_resource_group.wintech.name
+  location                 = azurerm_resource_group.wintech.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
@@ -67,15 +67,15 @@ resource "azurerm_storage_account" "example" {
   }
 }
 
-resource "azurerm_virtual_machine" "example" {
-  name                  = "example-vm"
-  location              = azurerm_resource_group.example.location
-  resource_group_name   = azurerm_resource_group.example.name
-  network_interface_ids = [azurerm_network_interface.example.id]
+resource "azurerm_virtual_machine" "wintech" {
+  name                  = "wintech-vm"
+  location              = azurerm_resource_group.wintech.location
+  resource_group_name   = azurerm_resource_group.wintech.name
+  network_interface_ids = [azurerm_network_interface.wintech.id]
   vm_size               = "Standard_DS1_v2"
 
   storage_os_disk {
-    name              = "example-osdisk"
+    name              = "wintech-osdisk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
@@ -89,7 +89,7 @@ resource "azurerm_virtual_machine" "example" {
   }
 
   os_profile {
-    computer_name  = "example-vm"
+    computer_name  = "wintech-vm"
     admin_username = "adminuser"
 
     admin_password = "P@ssw0rd1234!"
@@ -104,7 +104,7 @@ resource "azurerm_virtual_machine" "example" {
   }
 }
 
-resource "azurerm_network_interface_security_group_association" "example" {
-  network_interface_id      = azurerm_network_interface.example.id
-  network_security_group_id = azurerm_network_security_group.example.id
+resource "azurerm_network_interface_security_group_association" "wintech" {
+  network_interface_id      = azurerm_network_interface.wintech.id
+  network_security_group_id = azurerm_network_security_group.wintech.id
 }
